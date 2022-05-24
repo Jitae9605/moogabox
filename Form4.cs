1411a0580c72ff3f7eeb5de7d07c03fd45bf6b27
ynns1217 <ynns1217@naver.com>
2022-05-24 17:38:28 +0900

    tmpreservation 데이터 넣기 수정중+ 영화,시간 선택시 데이터 위치 안맞음 + 사진 파일위치 수정해야됨

--- Form4.cs
+++ Form4.cs
@@ -8,126 +8,373 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-using WinFormsApp1;
-
 
 namespace moogabox
 {
+    //SQL에서 시간불러와서 DateCompare함수 이용해서 구하기.
+    //btnTime01.Enabled = false;
     public partial class Form4 : Form
     {
+        private SqlConnection Con;
         public Form4()
         {
             InitializeComponent();
         }
+        private int EduNum = 0;
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            Con = new SqlConnection();
+            Con.ConnectionString = "Server=(local);database=MoogaBox;" +
+                "Integrated Security=true";
+            Con.Open();
 
+            string Nowtime = DateTime.Now.ToString("yyyy년 MM월 dd일 HH시 mm분");
+            lblTime.Text = Nowtime;
+
+            Btn_enabled();
+
+
+        }
+
+        private void Form4_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (Con.State == ConnectionState.Open)
+            {
+                Con.Close();
+            }
+        }
+        // 범죄도시 2
         private void btnTime01_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='CC01'", Con);
+
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie1.Image;
+            }
+            myRead01.Close();
+
+            string InsertSql = string.Format("insert into Mvname, Hall,StartTime TmpReservation select Mvname,Hall,StartTime from Movie");
+            var Com = new SqlCommand(InsertSql, Con);
+
             groupBox1.Visible = true;
+
         }
 
         private void btnTime02_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='CC02'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie1.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
+
         }
 
         private void btnTime03_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='CC03'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie1.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
+
         }
 
         private void btnTime04_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='CC04'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie1.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
         private void btnTime05_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='CC05'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie1.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
         private void btnTime06_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='CC06'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie1.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
+        //닥터스트레인지
         private void btnTime07_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='DR01'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie2.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
         private void btnTime08_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='DR02'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie2.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
         private void btnTime09_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='DR03'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie2.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
         private void btnTime010_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='DR04'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie2.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
         private void btnTime11_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='DR05'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie2.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
         private void btnTime12_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='DR06'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie2.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
         private void btnTime13_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='DR07'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie2.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
-        private void btnTime14_Click(object sender, EventArgs e)
-        {
-            groupBox1.Visible = true;
-        }
-
+        //쥬라기월드 -도미니언
         private void btnTime15_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='JW01'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie3.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
+
         }
 
         private void btnTime16_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='JW02'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie3.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
         private void btnTime17_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='JW03'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie3.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
         private void btnTime18_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='JW04'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie3.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
         private void btnTime19_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='JW05'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie3.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
         private void btnTime20_Click(object sender, EventArgs e)
         {
+            var Comm01 = new SqlCommand("select * from Movie where MvNum='JW06'", Con);
+            var myRead01 = Comm01.ExecuteReader();
+            if (myRead01.Read())
+            {
+                this.name.Text = myRead01[0].ToString();
+                this.time.Text = myRead01[2].ToString();
+                pbMovie.Image = pbMovie3.Image;
+            }
+            myRead01.Close();
+
             groupBox1.Visible = true;
         }
 
-        private void btnTime21_Click(object sender, EventArgs e)
+
+
+        private void bntOK_Click(object sender, EventArgs e)
         {
-            groupBox1.Visible = true;
+            //지정한 영화이름과 영화품번, 시간이 TmpReservation으로 들어간다
+
+
+
+            //좌석창으로 이동
+            Form5 form5 = new Form5();
+            form5.Show();
+            this.Hide();
+
         }
 
-		private void Form4_Load(object sender, EventArgs e)
-		{
+        private void pbMovie1_Click(object sender, EventArgs e)
+        {
 
-		}
-	}
+        }
+
+        private void Btn_enabled()
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            //첫 창으로 이동
+            var Comm = new SqlCommand("delete from TmpReservation", Con);
+            Comm.ExecuteNonQuery();
+
+            Con.Close();
+
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+        }
+    }
 }
--- Form5.cs
+++ Form5.cs
@@ -8,21 +8,381 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-using WinFormsApp1;
-
 
 namespace moogabox
 {
     public partial class Form5 : Form
     {
+        private string Constr = "Server=210.119.12.69; Uid=User1;Pwd=1234;database=MoogaBox;" + "Integrated Security=false"; //SQL 연결문자열
         public Form5()
         {
             InitializeComponent();
         }
+        int count = 0;
 
-		private void Form5_Load(object sender, EventArgs e)
-		{
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("최대 선택 좌석은 4개입니다.", "주의사항",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+        private void btnA1_Click_1(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "A1\n";
+                btnA1.BackColor = Color.Red;
+                count++;
+            }
+           else if(count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            
+        }
+        private void btnA2_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "A2\n";
+                btnA2.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
 
-		}
-	}
+        private void btnA3_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "A3\n";
+                btnA3.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+
+        private void btnA4_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "A4\n";
+                btnA4.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnA5_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "A5\n";
+                btnA5.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+        private void btnB1_Click_1(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "B1\n";
+                btnB1.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnB2_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "B2\n";
+                btnB2.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnB3_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "B3\n";
+                btnB3.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnB4_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "B4\n";
+                btnB4.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnB5_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "B5\n";
+                btnB5.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnC1_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "C1\n";
+                btnC1.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnC2_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "C2\n";
+                btnC2.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnC3_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "C3\n";
+                btnC3.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnC4_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "C4\n";
+                btnC4.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+         }
+
+        private void btnC5_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "C5\n";
+                btnC5.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnD1_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "D1\n";
+                btnD1.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+         }
+
+        private void btnD2_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "D2\n";
+                btnD2.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+         }
+
+        private void btnD3_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "D3\n";
+                btnD3.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnD4_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "D4\n";
+                btnD4.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnD5_Click(object sender, EventArgs e)
+        {
+            if (count < 4)
+            {
+                label2.Text += "D5\n";
+                btnD5.BackColor = Color.Red;
+                count++;
+            }
+            else if (count >= 4)
+            {
+                MessageBox.Show("최대 선택가능 좌석은 4개입니다.", "좌석선택오류", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+        private void btnPeople_Click(object sender, EventArgs e)
+        {
+            label3.Text = Convert.ToString(count);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnA1.BackColor = SystemColors.Control;
+            btnA2.BackColor = SystemColors.Control;
+            btnA3.BackColor = SystemColors.Control;
+            btnA4.BackColor = SystemColors.Control;
+            btnA5.BackColor = SystemColors.Control;
+            btnB1.BackColor = SystemColors.Control;
+            btnB2.BackColor = SystemColors.Control;
+            btnB3.BackColor = SystemColors.Control;
+            btnB4.BackColor = SystemColors.Control;
+            btnB5.BackColor = SystemColors.Control;
+            btnC1.BackColor = SystemColors.Control;
+            btnC2.BackColor = SystemColors.Control;
+            btnC3.BackColor = SystemColors.Control;
+            btnC4.BackColor = SystemColors.Control;
+            btnC5.BackColor = SystemColors.Control;
+            btnD1.BackColor = SystemColors.Control;
+            btnD2.BackColor = SystemColors.Control;
+            btnD3.BackColor = SystemColors.Control;
+            btnD4.BackColor = SystemColors.Control;
+            btnD5.BackColor = SystemColors.Control;
+
+            label2.Text = "";
+            label3.Text = "";
+            count = 0;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            //첫 창으로 이동
+            var Conn = new SqlConnection(Constr);
+            Conn.Open();
+
+            var Comm = new SqlCommand("delete from TmpReservation", Conn);
+            Comm.ExecuteNonQuery();
+
+            Conn.Close();
+
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+
+
+
+            //다음창으로 이동
+            Form6 form6 = new Form6();
+            form6.Show();
+            this.Hide();
+        }
+    }
 }
