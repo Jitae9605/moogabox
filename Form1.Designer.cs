362695dfc058779825f0ba735b1256b5579b4d1f
YC Kim <wcpekim@naver.com>
2022-05-26 11:34:57 +0900

    form0 UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,56 +29,84 @@ namespace moogabox
         /// </summary>
         private void InitializeComponent()
         {
-			this.btnInquiry = new System.Windows.Forms.Button();
-			this.btnticket = new System.Windows.Forms.Button();
-			this.btnStore = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// btnInquiry
-			// 
-			this.btnInquiry.Font = new System.Drawing.Font("맑은 고딕", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.btnInquiry.Location = new System.Drawing.Point(57, 60);
-			this.btnInquiry.Name = "btnInquiry";
-			this.btnInquiry.Size = new System.Drawing.Size(191, 280);
-			this.btnInquiry.TabIndex = 0;
-			this.btnInquiry.Text = "예매 조회";
-			this.btnInquiry.UseVisualStyleBackColor = true;
-			this.btnInquiry.Click += new System.EventHandler(this.btnInquiry_Click);
-			// 
-			// btnticket
-			// 
-			this.btnticket.Font = new System.Drawing.Font("맑은 고딕", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.btnticket.Location = new System.Drawing.Point(278, 60);
-			this.btnticket.Name = "btnticket";
-			this.btnticket.Size = new System.Drawing.Size(191, 280);
-			this.btnticket.TabIndex = 1;
-			this.btnticket.Text = "예매";
-			this.btnticket.UseVisualStyleBackColor = true;
-			this.btnticket.Click += new System.EventHandler(this.btnticket_Click);
-			// 
-			// btnStore
-			// 
-			this.btnStore.Font = new System.Drawing.Font("맑은 고딕", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.btnStore.Location = new System.Drawing.Point(498, 60);
-			this.btnStore.Name = "btnStore";
-			this.btnStore.Size = new System.Drawing.Size(191, 280);
-			this.btnStore.TabIndex = 2;
-			this.btnStore.Text = "매점";
-			this.btnStore.UseVisualStyleBackColor = true;
-			this.btnStore.Click += new System.EventHandler(this.btnStore_Click);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(748, 395);
-			this.Controls.Add(this.btnStore);
-			this.Controls.Add(this.btnticket);
-			this.Controls.Add(this.btnInquiry);
-			this.Name = "Form1";
-			this.Text = "메인메뉴";
-			this.Load += new System.EventHandler(this.Form1_Load);
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnInquiry = new System.Windows.Forms.Button();
+            this.btnticket = new System.Windows.Forms.Button();
+            this.btnStore = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnInquiry
+            // 
+            this.btnInquiry.Font = new System.Drawing.Font("맑은 고딕", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnInquiry.Location = new System.Drawing.Point(57, 103);
+            this.btnInquiry.Name = "btnInquiry";
+            this.btnInquiry.Size = new System.Drawing.Size(191, 237);
+            this.btnInquiry.TabIndex = 0;
+            this.btnInquiry.Text = "예매 조회";
+            this.btnInquiry.UseVisualStyleBackColor = true;
+            this.btnInquiry.Click += new System.EventHandler(this.btnInquiry_Click);
+            // 
+            // btnticket
+            // 
+            this.btnticket.Font = new System.Drawing.Font("맑은 고딕", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnticket.Location = new System.Drawing.Point(278, 103);
+            this.btnticket.Name = "btnticket";
+            this.btnticket.Size = new System.Drawing.Size(191, 237);
+            this.btnticket.TabIndex = 1;
+            this.btnticket.Text = "예매";
+            this.btnticket.UseVisualStyleBackColor = true;
+            this.btnticket.Click += new System.EventHandler(this.btnticket_Click);
+            // 
+            // btnStore
+            // 
+            this.btnStore.Font = new System.Drawing.Font("맑은 고딕", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnStore.Location = new System.Drawing.Point(498, 103);
+            this.btnStore.Name = "btnStore";
+            this.btnStore.Size = new System.Drawing.Size(191, 237);
+            this.btnStore.TabIndex = 2;
+            this.btnStore.Text = "매점";
+            this.btnStore.UseVisualStyleBackColor = true;
+            this.btnStore.Click += new System.EventHandler(this.btnStore_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(747, 74);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(748, 395);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 395);
+            this.Controls.Add(this.btnStore);
+            this.Controls.Add(this.btnticket);
+            this.Controls.Add(this.btnInquiry);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "메인메뉴";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
 
         }
 
@@ -87,5 +115,7 @@ namespace moogabox
         private System.Windows.Forms.Button btnInquiry;
         private System.Windows.Forms.Button btnticket;
         private System.Windows.Forms.Button btnStore;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
